dfe8e01b0040ddf3218cd130f3222c878ad65f66
Gustavo Pontes <69886395+GustavoPontess@users.noreply.github.com>
2021-09-13 20:33:45 -0300

    Add files via upload

---
+++ Prova/crud/inc/config.php
@@ -0,0 +1,11 @@
+<?php
+//Banco de dados web
+define("SERVIDOR", "mysql:host=fdb28.awardspace.net;port=3306; dbname=3542799_pontes; charset=utf8");
+define("USUARIO", "3542799_pontes");
+define("SENHA", "***REMOVED***");
+
+//Banco de dados local
+//define("SERVIDOR", "mysql:host=localhost; dbname=prova; charset=utf8");
+//define("USUARIO", "root");
+//define("SENHA", "");
+
