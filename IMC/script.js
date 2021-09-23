3b9963301f896d36d2f706121b3caf4a707a02e1
Gustavo Pontes <69886395+GustavoPontess@users.noreply.github.com>
2021-09-23 15:23:06 -0300

    Update script.js

--- IMC/script.js
+++ IMC/script.js
@@ -2,26 +2,29 @@
 function Imc() {
 var nome = document.getElementById('nome').value;
 var peso = document.getElementById('peso').value;
-var altura = document.getElementById('altura').value;
+var altura = document.getElementById('altura').value().replace(",",".");
 if(nome ==''||peso==''||altura==''){
     window.alert('Preencha todos os campos!!!')
     return false;
 }
 var x = (peso/(altura*altura)).toFixed(2);
+var peso_minimo = altura*altura*18.5.toFixed(2);
+var peso_maximo = altura*altura*24.9.toFixed(2);
+
 if(x<17){
-    document.getElementById('resultado').innerHTML=nome+' você está muito abaixo do peso, '+x+'kg/m2';
+    document.getElementById('resultado').innerHTML=nome+' você está muito abaixo do peso, com IMC '+ x +'Procure manter o seu peso entre '+peso_minimo+'kg é '+peso_maximo+'kg.';
 }else if(x>=17 && x <=18.49){
-    document.getElementById('resultado').innerHTML=nome+' você está abaixo do peso, '+x+'kg/m2';
+    document.getElementById('resultado').innerHTML=nome+' você está abaixo do peso, com IMC '+ x +'Procure manter o seu peso entre '+peso_minimo+'kg é '+peso_maximo+'kg.';
 }else if(x>=18.5 && x <= 24.99){
-    document.getElementById('resultado').innerHTML=nome+' você está com o peso normal, '+x+'kg/m2';
+    document.getElementById('resultado').innerHTML=nome+' você está com o peso normal, com IMC '+ x +'Procure manter o seu peso entre '+peso_minimo+'kg é '+peso_maximo+'kg.';
 }else if(x>=25 && x <= 29.99){
-    document.getElementById('resultado').innerHTML=nome+' você está acima do peso, '+x+'kg/m2';
+    document.getElementById('resultado').innerHTML=nome+' você está acima do peso, com IMC '+ x +'Procure manter o seu peso entre '+peso_minimo+'kg é '+peso_maximo+'kg.';
 }else if(x>=30 && x <= 34.99){
-    document.getElementById('resultado').innerHTML=nome+' você tem obesidade nível 1, '+x+'kg/m2';
+    document.getElementById('resultado').innerHTML=nome+' você tem obesidade nível 1, com IMC '+ x +'Procure manter o seu peso entre '+peso_minimo+'kg é '+peso_maximo+'kg.';
 }else if(x>=35 && x <= 39.99){
-    document.getElementById('resultado').innerHTML=nome+' você tem obesidade nível 2, '+x+'kg/m2';
+    document.getElementById('resultado').innerHTML=nome+' você tem obesidade nível 2, com IMC '+ x +'Procure manter o seu peso entre '+peso_minimo+'kg é '+peso_maximo+'kg.';
 }else if(x >= 40){
-    document.getElementById('resultado').innerHTML=nome+' você tem obesidade nível 3, '+x+'kg/m2';
+    document.getElementById('resultado').innerHTML=nome+' você tem obesidade nível 3, com IMC '+ x +'Procure manter o seu peso entre '+peso_minimo+'kg é '+peso_maximo+'kg.';
 }
 }
 // window.alert('olá mundo!')
